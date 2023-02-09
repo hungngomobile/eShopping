c0bb715bf2e2c1c451fbddf5f3b4a97997f1c10a
rahulsahay19 <rahulsahay19@hotmail.com>
2023-02-09 21:09:55 +0530

    Rabbit MQ Changes

--- Services/Basket/Basket.API/Startup.cs
+++ Services/Basket/Basket.API/Startup.cs
@@ -5,6 +5,7 @@ using Basket.Core.Repositories;
 using Basket.Infrastructure.Repositories;
 using Discount.Grpc.Protos;
 using HealthChecks.UI.Client;
+using MassTransit;
 using MediatR;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
@@ -43,6 +44,14 @@ public class Startup
         });
         services.AddHealthChecks()
             .AddRedis(Configuration["CacheSettings:ConnectionString"], "Redis Health", HealthStatus.Degraded);
+        services.AddMassTransit(config =>
+        {
+            config.UsingRabbitMq((ct, cfg)=>
+            {
+                cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+            });
+        });
+        services.AddMassTransitHostedService();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
