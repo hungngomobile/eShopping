7d085b6b4bce4204e74b10ac76d87b87c24f38c8
rahulsahay19 <rahulsahay19@hotmail.com>
2023-02-11 17:23:30 +0530

    Ordering Completed

--- Infrastructure/EventBus.Messages/Common/EventBusConstants.cs
+++ Infrastructure/EventBus.Messages/Common/EventBusConstants.cs
@@ -1,6 +1,6 @@
 namespace EventBus.Messages.Common;
 
-public class EventBusConstant
+public class EventBusConstants
 {
     //This queue name will come in rabbit mq management portal
     public const string BasketCheckoutQueue = "basketcheckout-queue";
--- Services/Ordering/Ordering.API/Startup.cs
+++ Services/Ordering/Ordering.API/Startup.cs
@@ -1,6 +1,9 @@
+using EventBus.Messages.Common;
 using HealthChecks.UI.Client;
+using MassTransit;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.OpenApi.Models;
+using Ordering.API.EventBusConsumer;
 using Ordering.Application.Extensions;
 using Ordering.Infrastructure.Data;
 using Ordering.Infrastructure.Extensions;
@@ -23,11 +26,27 @@ public class Startup
         services.AddApplicationServices();
         services.AddInfraServices(Configuration);
         services.AddAutoMapper(typeof(Startup));
+        services.AddScoped<BasketOrderingConsumer>();
         services.AddSwaggerGen(c =>
         {
             c.SwaggerDoc("v1", new OpenApiInfo {Title = "Ordering.API", Version = "v1"});
         });
         services.AddHealthChecks().Services.AddDbContext<OrderContext>();
+        services.AddMassTransit(config =>
+        {
+            //Mark this as consumer
+            config.AddConsumer<BasketOrderingConsumer>();
+            config.UsingRabbitMq((ctx, cfg)=>
+            {
+                cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+                //provide the queue name with consumer settings
+                cfg.ReceiveEndpoint(EventBusConstants.BasketCheckoutQueue, c =>
+                {
+                    c.ConfigureConsumer<BasketOrderingConsumer>(ctx);  
+                });
+            });
+        });
+        services.AddMassTransitHostedService();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
