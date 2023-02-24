19aff39d22bc5368556c980e4026945eb255e43d
rahulsahay19 <rahulsahay19@hotmail.com>
2023-02-25 00:37:14 +0530

    Implemented IPagination Model

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { IPagination } from './shared/models/pagination';
+import { IProduct } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +10,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title = 'eShopping';
-  products: any[] = [];
+  products: IProduct[] = [];
 
   constructor(private http: HttpClient){}
   ngOnInit(): void {
-    this.http.get('http://localhost:9010/Catalog/GetProductsByBrandName/Adidas').subscribe({
-      next:(response:any) => {
-        this.products = response,
+    this.http.get<IPagination<IProduct[]>>('http://localhost:9010/Catalog/GetAllProducts').subscribe({
+      next:response => {
+        this.products = response.data,
         console.log(response)
       },
       error: error=>console.log(error),
